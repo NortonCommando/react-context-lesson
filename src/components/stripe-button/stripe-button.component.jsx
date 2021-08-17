8bb2afc0cf9dec4425c50bd3cbe58c4b9bb99986
Kepa Mola <k.mola@e-coordina.com>
2021-08-17 09:52:41 +0200

    Credential change

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey = 'pk_test_51JIs2TBnkYaiN09EM7uCb52ZFF99LmcYpAkNjtpUBYQwHrdHP6Fh5pdiJN1C4dRJw54mY8Rt8MQlIf9o8rkQQHwP00sWAReG8N';
 
   const onToken = token => {
     console.log(token);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,15 +1,15 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyClg2hA4vqr5pDv76l-Xo9uC0aFhHaqOsw",
+  authDomain: "crwn-react-db-c3158.firebaseapp.com",
+  projectId: "crwn-react-db-c3158",
+  storageBucket: "crwn-react-db-c3158.appspot.com",
+  messagingSenderId: "168872814340",
+  appId: "1:168872814340:web:01f5ee8d3cfade21d69251",
+  measurementId: "G-CZFHG88JV5",
 };
 
 firebase.initializeApp(config);
@@ -29,10 +29,10 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         displayName,
         email,
         createdAt,
-        ...additionalData
+        ...additionalData,
       });
     } catch (error) {
-      console.log('error creating user', error.message);
+      console.log("error creating user", error.message);
     }
   }
 
@@ -43,7 +43,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
